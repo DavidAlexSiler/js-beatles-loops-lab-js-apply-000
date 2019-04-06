134228c6862c57be543a45eed71521fba5b21245
Alex Siler <davidalexsiler@gmail.com>
2019-04-06 05:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var iLoveTheBeatles = function(number){
   do{
     i++;
     if(number > 15){
-      return "I love the Beatles";
+      return arr.push("I love the Beatles");
     }
    arr.push('I love the Beatles!');
     } while(i <= number);
